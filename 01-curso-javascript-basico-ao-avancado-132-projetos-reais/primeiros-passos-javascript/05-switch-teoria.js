6568b787829fcc635e32e9fb664ff1ad839bd86d
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-07-28 13:21:06 -0300

    Fiz anotações da aula teórica sobre Switch

---
+++ 01-curso-javascript-basico-ao-avancado-132-projetos-reais/primeiros-passos-javascript/05-switch-teoria.js
@@ -0,0 +1,19 @@
+/*
+O Switch é uma estrutura de controle em JavaScript que permite executar diferentes partes do código com base no valor de uma expressão.
+É uma alternativa ao uso de muitos if...else, tornando o código mais organizado e legível quando temos várias condições para verificar.
+A estrutura do switch compara o valor de uma expressão com valores de case especificados e executa o bloco de código associado ao case correspondente.
+
+
+Estrutura básica de switch:
+ switch (expressao) {
+    case valor1:
+        //bloco de código a executar quando expressao === valor1
+        break;
+    case valor2:
+        //bloco de código a executar quando expressao === valor2
+        break;
+    //você pode adicionar quantos cases forem necessários
+    default:
+        //bloco de código a executar quando nenhum dos cases anteriores corresponder
+ }
+*/
